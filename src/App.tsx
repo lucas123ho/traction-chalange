ae9febc4efb9e0000fa96ba20742af6857b4c202
Lucas Santos Lima <lucas.galrao@hotmail.com>
2023-01-29 19:43:02 -0300

    configure app base

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import Home from "pages/Home";
-import { Route, Routes } from "react-router-dom";
-import GlobalStyle from "styles/global";
+import { ConfigProvider } from 'antd';
+import 'antd/dist/reset.css';
+import { Provider } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import Home from 'pages/Home';
+
+import store from 'store';
+
+import GlobalStyle from 'styles/global';
+import theme from 'styles/theme';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ConfigProvider
+          theme={{
+            token: {
+              fontFamily: 'Poppins, sans-serif'
+            }
+          }}
+        >
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </ConfigProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
